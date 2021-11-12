9d5b8591c3bbcff353771c6f03fcbf977c32a3f0
manshad <manshadchangampalli010@gmail.com>
2021-11-12 14:34:55 +0530

    the webpage is almost finished

--- src/Components/Tittle/Tittle.jsx
+++ src/Components/Tittle/Tittle.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import playIcon from '../../Assets/img/play-circle.svg'
 import appleIcon from '../../Assets/img/apple.svg'
 import './Tittle.scss'
+import Aos from "aos"
+import 'aos/dist/aos.css'
 
 function Tittle() {
+    useEffect(() => {
+    
+        Aos.init({duration:2000});
+    },[])
     return (
         <div className="tittle">
             <h1 className="tittle-h1">Build your <br /> Next <span>Product</span> <br /> with Deski.</h1>
-            <p className="sub-tittle-p">Deski delivered blazing fast, striking soludtion</p>
+            <p data-aos="fade-up" className="sub-tittle-p">Deski delivered blazing fast, striking soludtion</p>
             <div className="buttons">
-                <div className="applestore">
+                <div data-aos="fade-up" data-aos-duration="500" className="applestore">
                     <div className="apple-icon">
                         <img className="apple-icon-img" src={appleIcon} alt="" />
                     </div>
@@ -18,7 +24,7 @@ function Tittle() {
                         <p className="app-store-text">App store</p>
                     </div>
                 </div>
-                <div className="seeintro">
+                <div data-aos="fade-up" className="seeintro">
                     <img src={playIcon} alt="" />
                     <p>See intro Video</p>
                 </div>
